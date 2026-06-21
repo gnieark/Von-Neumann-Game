9adc75b24ed2cc50e4d7700c250796c8e1796492
gnieark <gnieark@tinad.fr>
2026-06-21 15:59:56 +0200

    fix responsive

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 const SESSION_COOKIE = 'vn_session';
 const LANGUAGE_COOKIE = 'vn_lang';
-const ASSET_VERSION = '20260621-storage-container-rename-form';
+const ASSET_VERSION = '20260621-inventory-rename-form-layout';
 
 $projectRoot = dirname(__DIR__);
 $factory = new AppFactory($projectRoot);
